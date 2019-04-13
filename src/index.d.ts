6e039c01d0ff5a489ab9e6504c28a9613c5d9b0e
Kyle Gray <kgray1497@gmail.com>
2019-04-13 11:46:30 -0400

    adding type defs

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+export interface AmplifyRouterProps {
+    amplifyConfig?: object;
+    componentOverrides?: Array<React.ReactType>;
+    homeRoute?: String
+}
+
+declare const AmplifyRouter: React.FunctionComponent<AmplifyRouterProps>;
+
+export default AmplifyRouter;
