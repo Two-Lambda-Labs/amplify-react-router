cb74e53d27586ec9664abf124252d7537110eaab
Kyle <kgray1497@gmail.com>
2019-04-29 18:32:37 -0400

    feat: added ability to send routerProps to the containg router element

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,7 @@
 export interface AmplifyRouterProps {
     amplifyConfig?: object;
     componentOverrides?: Array<React.ReactType>;
+    routerProps: React.HTMLAttributes<HTMLDivElement>;
     hide?: Array<React.ReactType>;
     homeRoute?: String
 }
